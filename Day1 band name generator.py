cceb32ea4aa827590ddcfcff6fe85e7eb4105382
Marvin-Lee Marshall <87153147+Bingoboy2@users.noreply.github.com>
2023-12-05 17:42:05 +0200

    Day 1
    My first code which is a band name generator

---
+++ Day1 band name generator.py
@@ -0,0 +1,10 @@
+print("Welcome to your band name generator :)")
+
+food = input("What is the last food you ate?\n")
+city = input("Where were you born?\n")
+
+print("Your band name is " + food + " " + city)
+
+
+
+
